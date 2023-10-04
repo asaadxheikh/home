023962ce32c56ed6b57337881060aa878571710c
Sh Asaad <asaadxheikh_1@yahoo.com>
2023-10-04 14:32:32 +0500

    air

---
+++ ios/homeWidget/homeWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  homeWidgetBundle.swift
+//  homeWidget
+//
+//  Created by asaad on 15/03/2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct homeWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        homeWidget()
+        homeWidgetLiveActivity()
+    }
+}
